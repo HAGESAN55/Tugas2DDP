8b37454ddae6533b9ce9280dd28ba6c68a118e0c
Fadli Fadillah <55126894+HAGESAN55@users.noreply.github.com>
2020-12-25 10:24:17 +0700

    Update Tugas 2 DDP.py

--- Tugas 2 DDP.py
+++ Tugas 2 DDP.py
@@ -1,20 +1,17 @@
-# import label_a
-# try:
-    print("***** SELAMAT DATANG DI NF BANK *****")
-    print("Menu: ")
-    print("[1] Buka Rekening Bank\n[2] Setoran Tunai\n[3] Tarik Tunai\n[4] Transfer\n[5] Lihat Daftar Transfer\n[6] Keluar")
-    while True:
-        pilih = eval(input("Masukkan menu pilihan anda: "))
-        if pilih == 1:
-            nama = input("Masukkan nama anda: ")
-            setoran = eval(input("Masukkan Setoran Awal: "))
-            import random, string
-            norek =  "REK" + ''.join(random.choice(string.digits) for _ in range(3))
-            print("Pembukaan dengan nomor rekening", norek, "atas nama", nama, "berhasil.")
-            import goto
+print("***** SELAMAT DATANG DI NF BANK *****")
+print("Menu: ")
+print("[1] Buka Rekening Bank\n[2] Setoran Tunai\n[3] Tarik Tunai\n[4] Transfer\n[5] Lihat Daftar Transfer\n[6] Keluar")
+while True:
+    pilih = eval(input("Masukkan menu pilihan anda: "))
+    if pilih == 1:
+        nama = input("Masukkan nama anda: ")
+        setoran = eval(input("Masukkan Setoran Awal: "))
+        import random, string
+        norek =  "REK" + ''.join(random.choice(string.digits) for _ in range(3))
+        print("Pembukaan dengan nomor rekening", norek, "atas nama", nama, "berhasil.")
             
-        elif pilih == 6:
-            print("Terima kasih atas kunjungan anda")
-            break
-        else:
-            print("Pilihan anda salah. Ulangi.")
+    elif pilih == 6:
+        print("Terima kasih atas kunjungan anda")
+        break
+    else:
+        print("Pilihan anda salah. Ulangi.")
